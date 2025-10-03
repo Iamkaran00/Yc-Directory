8da64a35321decfca604d6fc4e24a565b57a3f95
Karan Sahu <iamironman892465@gmail.com>
2025-10-03 11:33:30 +0530

    removed import

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import {withSentryConfig} from "@sentry/nextjs";
  
 import type { NextConfig } from "next";
 
 const NextConfig = {
 
-  /* config options here */
   images:{
     dangerouslyAllowSVG : true,
     
